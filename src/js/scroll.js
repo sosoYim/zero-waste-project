f5756a33c764087122ad232733b3dcb1fc4ac88c
jkpark104 <jkrang104@gmail.com>
2021-08-31 17:29:18 +0900

    feat: Add section intro

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,70 +1,39 @@
 gsap.registerPlugin(ScrollTrigger);
 
-let sections = gsap.utils.toArray(".intro__slide");
-
-gsap.to(sections, {
-  xPercent: -100 * (sections.length - 1),
-  ease: "none",
+gsap.to(".state__content", {
   scrollTrigger: {
-    trigger: ".intro__container",
+    markers: true,
+    trigger: ".state__content",
+    scrub: true,
     pin: true,
-    scrub: 1,
-    snap: directionalSnap(1 / (sections.length - 1)),
-    // base vertical scrolling on how wide the container is so it feels more natural.
-    end: "+=3500"
+    start: "center center",
+    end: "bottom -100%",
+    toggleClass: "is-active",
+    ease: "power2"
   }
 });
 
-
-// helper function for causing the sections to always snap in the direction of the scroll (next section) rather than whichever section is "closest" when scrolling stops.
-function directionalSnap(increment) {
-  let snapFunc = gsap.utils.snap(increment);
-  return (raw, self) => {
-    let n = snapFunc(raw);
-    return Math.abs(n - raw) < 1e-4 || (n < raw) === self.direction < 0 ? n : self.direction < 0 ? n - increment : n + increment;
-  };
-}
-
-
-//---------------------------------------------------------
-function animateFrom(elem, direction) {
-  direction = direction || 1;
-  var x = 0,
-      y = direction * 100;
-  if(elem.classList.contains("gs_reveal_fromLeft")) {
-    x = -100;
-    y = 0;
-  } else if (elem.classList.contains("gs_reveal_fromRight")) {
-    x = 100;
-    y = 0;
-  }
-  elem.style.transform = "translate(" + x + "px, " + y + "px)";
-  elem.style.opacity = "0";
-  gsap.fromTo(elem, {x: x, y: y, autoAlpha: 0}, {
-    duration: 1.25, 
-    x: 0,
-    y: 0, 
-    autoAlpha: 1, 
-    ease: "expo", 
-    overwrite: "auto"
-  });
-}
-
-function hide(elem) {
-  gsap.set(elem, {autoAlpha: 0});
-}
-
-document.addEventListener("DOMContentLoaded", function() {
-  gsap.registerPlugin(ScrollTrigger);
-  
-  gsap.utils.toArray(".gs_reveal").forEach(function(elem) {
-    hide(elem); // assure that the element is hidden when scrolled into view
-    
-    ScrollTrigger.create({
-      trigger: elem,
-      onEnter: function() { animateFrom(elem) }, 
-      onEnterBack: function() { animateFrom(elem, -1) },
-      onLeave: function() { hide(elem) } // assure that the element is hidden when scrolled into view
-    });
-  });
+gsap.to(".state__image img", {
+  scrollTrigger: {
+    trigger: ".hero",
+    scrub: 0.5,
+    start: "top bottom",
+    end: "bottom -300%",
+    ease: "power2"
+  },
+  y: "-30%"
 });
+
+// gsap.utils.toArray(".intro").forEach((panel, i) => {
+//   ScrollTrigger.create({
+//     trigger: panel,
+//     start: "top top", 
+//     pin: true, 
+//     pinSpacing: false
+//   });
+// });
+
+
+// ScrollTrigger.create({
+//   snap: 1 / 4 // snap whole page to the closest section!
+// });
